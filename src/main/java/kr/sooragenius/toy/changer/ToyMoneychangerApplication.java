d5dd1068a9f0f57347472229a6175cddd1020c61
ohgillwhan <soora_genius@naver.com>
2021-03-02 21:28:55 +0900

    init : 프로젝트 생성

---
+++ src/main/java/kr/sooragenius/toy/changer/ToyMoneychangerApplication.java
@@ -0,0 +1,13 @@
+package kr.sooragenius.toy.changer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyMoneychangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyMoneychangerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/sooragenius/toy/changer/ToyMoneychangerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.sooragenius.toy.changer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyMoneychangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
