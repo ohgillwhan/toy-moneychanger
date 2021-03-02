c06323765406d2798199a2afa79f9962ec2f6a3a
ohgillwhan <soora_genius@naver.com>
2021-03-02 23:47:57 +0900

    feat(CurrencyAPIService) : CurrencyRequest를 통해 결과값을 가져온 다음에 CurrencyResponse를 Return하도록 설계 중

---
+++ src/main/java/kr/sooragenius/toy/changer/service/CurrencyAPIService.java
@@ -0,0 +1,10 @@
+package kr.sooragenius.toy.changer.service;
+
+import kr.sooragenius.toy.changer.request.CurrencyRequest;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CurrencyAPIService {
+    private final CurrencyRequest currencyRequest;
+
+}
