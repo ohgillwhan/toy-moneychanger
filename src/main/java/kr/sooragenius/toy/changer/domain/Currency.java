ef86843a0f2883625814731900c30ec99b245f6e
ohgillwhan <soora_genius@naver.com>
2021-03-04 23:49:21 +0900

    fix(Currency) : 생성자 notnull이여야 한다.

--- src/main/java/kr/sooragenius/toy/changer/domain/Currency.java
+++ src/main/java/kr/sooragenius/toy/changer/domain/Currency.java
@@ -2,6 +2,7 @@ package kr.sooragenius.toy.changer.domain;
 
 import kr.sooragenius.toy.changer.enums.CurrencyType;
 import lombok.Getter;
+import org.springframework.util.Assert;
 
 @Getter
 public class Currency {
@@ -10,6 +11,8 @@ public class Currency {
     private double rate;
 
     public Currency(String source, String fromTo, double rate) {
+        Assert.notNull(source, "source는 notNull 여야 합니다.");
+        Assert.notNull(fromTo, "fromTo는 notNull 여야 합니다.");
         this.from = CurrencyType.valueOf(source);
         this.to = CurrencyType.valueOf(fromTo.replace(source, ""));
         this.rate = rate;
