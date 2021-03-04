822b16de457ea2f3f16ac805e53791538a5cf114
ohgillwhan <soora_genius@naver.com>
2021-03-05 00:23:53 +0900

    refact(.API) : API 패키지 리팩토링

--- src/main/java/kr/sooragenius/toy/changer/api/controller/APIMockController.java
+++ src/main/java/kr/sooragenius/toy/changer/api/controller/APIMockController.java
@@ -1,10 +1,9 @@
-package kr.sooragenius.toy.changer.controller;
+package kr.sooragenius.toy.changer.api.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
