4340f3d96d1360693e0dc6e027014b1e2e061ccc
ohgillwhan <soora_genius@naver.com>
2021-03-05 00:41:02 +0900

    refact(CurrencyAPICronTest) : 함수 호출 간결하게 변경

--- src/test/java/kr/sooragenius/toy/changer/api/cron/CurrencyAPICronTest.java
+++ src/test/java/kr/sooragenius/toy/changer/api/cron/CurrencyAPICronTest.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @WebMvcTest(properties = {"currency.api.cron.expression=*/5 * * * * *"})
 @Import(
@@ -20,10 +20,10 @@ class CurrencyAPICronTest {
 
     @Test
     void 시작시_한번_5초뒤_한번_호출이_되어_총_2번_되어야_한다() throws InterruptedException {
-        verify(currencyAPICaller, VerificationModeFactory.atLeast(1))
+        verify(currencyAPICaller, atMostOnce())
                 .call();
         Thread.sleep(5000);
-        verify(currencyAPICaller, VerificationModeFactory.atLeast(2))
+        verify(currencyAPICaller, atLeast(2))
                 .call();
     }
 
