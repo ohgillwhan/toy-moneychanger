53a1b39bb75a0894b07d6a09e9ff20593517fa3c
ohgillwhan <soora_genius@naver.com>
2021-03-05 00:39:05 +0900

    test(CurrencyAPICronTest) : 시작시, 시간지난뒤 호출되는지 테스트

---
+++ src/test/java/kr/sooragenius/toy/changer/api/cron/CurrencyAPICronTest.java
@@ -0,0 +1,30 @@
+package kr.sooragenius.toy.changer.api.cron;
+
+import kr.sooragenius.toy.changer.api.service.CurrencyAPICaller;
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.verification.VerificationModeFactory;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+
+import static org.mockito.Mockito.verify;
+
+@WebMvcTest(properties = {"currency.api.cron.expression=*/5 * * * * *"})
+@Import(
+        CurrencyAPICron.class
+)
+class CurrencyAPICronTest {
+    @MockBean
+    private CurrencyAPICaller currencyAPICaller;
+
+
+    @Test
+    void 시작시_한번_5초뒤_한번_호출이_되어_총_2번_되어야_한다() throws InterruptedException {
+        verify(currencyAPICaller, VerificationModeFactory.atLeast(1))
+                .call();
+        Thread.sleep(5000);
+        verify(currencyAPICaller, VerificationModeFactory.atLeast(2))
+                .call();
+    }
+
+}
