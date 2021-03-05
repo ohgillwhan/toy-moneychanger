84914ca5f66d0221eedbf8e56aa77ba85d071c46
ohgillwhan <soora_genius@naver.com>
2021-03-06 00:08:10 +0900

    fix(Application) : EnableRetry 추가

--- src/main/java/kr/sooragenius/toy/changer/Application.java
+++ src/main/java/kr/sooragenius/toy/changer/Application.java
@@ -2,11 +2,13 @@ package kr.sooragenius.toy.changer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableRetry
 public class Application {
 
     public static void main(String[] args) {
